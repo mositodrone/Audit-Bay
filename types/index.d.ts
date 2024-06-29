76907fc5abb29f9d3b9dc648cf4d587c546fed1c
moses <mositodrone@gmail.com>
2024-06-29 01:45:31 +0100

    Adding project files

---
+++ types/index.d.ts
@@ -0,0 +1,82 @@
+/* eslint-disable no-unused-vars */
+
+// ====== USER PARAMS
+declare type CreateUserParams = {
+  clerkId: string;
+  email: string;
+  username: string;
+  firstName: string;
+  lastName: string;
+  photo: string;
+};
+
+declare type UpdateUserParams = {
+  firstName: string;
+  lastName: string;
+  username: string;
+  photo: string;
+};
+
+// ====== IMAGE PARAMS
+declare type AddProductParams = {
+  product: {
+    title: string;
+    publicId: string;
+    transformationType: string;
+    width: number;
+    height: number;
+    config: any;
+    secureURL: string;
+    transformationURL: string;
+    aspectRatio: string | undefined;
+    prompt: string | undefined;
+    color: string | undefined;
+  };
+  userId: string;
+  path: string;
+};
+
+declare type UpdateProductParams = {
+  product: {
+    _id: string;
+    title: string;
+    publicId: string;
+    transformationType: string;
+    width: number;
+    height: number;
+    config: any;
+    secureURL: string;
+    transformationURL: string;
+    aspectRatio: string | undefined;
+    prompt: string | undefined;
+    color: string | undefined;
+  };
+  userId: string;
+  path: string;
+};
+
+// ====== TRANSACTION PARAMS
+declare type CheckoutTransactionParams = {
+  plan: string;
+  credits: number;
+  amount: number;
+  buyerId: string;
+};
+
+declare type CreateTransactionParams = {
+  stripeId: string;
+  amount: number;
+  credits: number;
+  plan: string;
+  buyerId: string;
+  createdAt: Date;
+};
+
+// declare type ProductFormProps = {
+//   action: "Add" | "Update";
+//   userId: string;
+//   type: TransformationTypeKey;
+//   creditBalance: number;
+//   data?: IImage | null;
+//   config?: Transformations | null;
+// };
